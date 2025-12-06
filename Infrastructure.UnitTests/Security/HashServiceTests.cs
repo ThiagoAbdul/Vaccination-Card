803337befcce3fc882b392d98db2d96a1e74289a
ThiagoAbdul <thiagoabdul10@gmail.com>
2025-12-05 22:24:15 -0300

    test: Testado a comparação dos Hashs determinísticos

---
+++ Infrastructure.UnitTests/Security/HashServiceTests.cs
@@ -0,0 +1,36 @@
+﻿using Infrastructure.Security;
+using Newtonsoft.Json.Linq;
+
+namespace Infrastructure.UnitTests.Security;
+
+public class HashServiceTests
+{
+    private HashService _hashService = new();
+
+    [Theory]
+    [InlineData("smallValue")]
+    [InlineData("BigValueksdkdksj fdslkfkldkjfdkjffjk dfkd")]
+    public void Shold_ReturnTrue_When_ValuesAreEquals(string value)
+    {
+
+        string plain = value;
+        string hash = _hashService.GenerateDeterministicHash(value);
+
+        var equals = _hashService.Compare(input: plain, hash: hash);
+
+        Assert.True(equals);
+    }
+
+    [Theory]
+    [InlineData("smallValue")]
+    [InlineData("BigValueksdkdksj fdslkfkldkjfdkjffjk dfkd")]
+    public void Shold_ReturnFalse_When_ValuesAreDifferents(string value)
+    {
+        string plain = "other value";
+        string hash = _hashService.GenerateDeterministicHash(value);
+
+        var equals = _hashService.Compare(input: plain, hash: hash);
+
+        Assert.False(equals);
+    }
+}
---
+++ tests/Application.UnitTests/Features/Persons/Commands/CreatePersonCommandValidator.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.UnitTests.Features.Persons.Commands;
+
+internal class CreatePersonCommandValidator
+{
+}
