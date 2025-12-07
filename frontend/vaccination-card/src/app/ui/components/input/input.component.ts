d5c0d94319c998fd3ed3164f9581508d2af7ed2d
ThiagoAbdul <thiagoabdul10@gmail.com>
2025-12-07 12:22:15 -0300

    componente de input

---
+++ frontend/vaccination-card/src/app/ui/components/input/input.component.ts
@@ -0,0 +1,85 @@
+import { CommonModule } from '@angular/common';
+import { Component, ElementRef, inject, input, output } from '@angular/core';
+import { ControlValueAccessor, FormsModule, NgControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-input',
+  standalone: true,
+  imports: [FormsModule, CommonModule],
+  templateUrl: './input.component.html',
+  styleUrl: './input.component.scss'
+})
+export class InputComponent implements ControlValueAccessor {
+
+
+  protected value: string = ""
+  label = input<string>()
+  placeholder = input("", { transform: (value: string | undefined) => value || "" })
+  readOnly = input(false)
+  type = input<string>()
+  private ngControl = inject(NgControl, { optional: true })
+
+  errorMessage = input<string>()
+
+  get controlErrorMessage(): string | undefined {
+    const errors = this.ngControl?.errors
+    if (errors) {
+      if (errors["customMessage"])
+        return errors["customMessage"]
+      if (errors["required"])
+        return "Campo obrigatório"
+    }
+
+    return undefined
+  }
+
+  protected hasError = false
+
+  touched = false
+
+  blur = output<InputComponent>()
+
+
+
+  constructor(protected elRef: ElementRef) {
+    if (this.ngControl)
+      this.ngControl.valueAccessor = this
+
+
+  }
+
+  onBlur() {
+    this.touched = true
+    this.blur.emit(this)
+  }
+
+
+  onValueChange(value: string) {
+    this.onChange(value);
+    this.onTouched();
+  }
+
+
+
+  // ControlValueAccessor methods
+  writeValue(value: string): void {
+    this.value = value;
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+  setDisabledState?(isDisabled: boolean): void {
+    this.isDisabled = isDisabled;
+  }
+
+  protected onChange(value: string) { }
+  protected onTouched() { };
+  protected isDisabled: boolean = false;
+
+}
