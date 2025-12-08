4801471edd483e5ce2a83fc13a13b41861373f49
ThiagoAbdul <thiagoabdul10@gmail.com>
2025-12-08 00:03:40 -0300

    fix: (Main API) Padronizado o DTO de response da entidade Person

--- backend/VaccinationCard/src/Application/Features/People/Queries/PersonResponse.cs
+++ backend/VaccinationCard/src/Application/Features/People/Queries/PersonResponse.cs
@@ -7,8 +7,7 @@ namespace Application.Features.People.Queries;
 public class PersonResponse(Person person)
 {
     public Guid Id { get; set; } = person.Id;
-    public string FirstName { get; set; } = person.Name.FirstName;
-    public string LastName { get; set; } = person.Name.LastName;
+    public Name Name { get; set; } = person.Name;
     public Gender Gender { get; set; } = person.Gender;
     public DateOnly BirthDate { get; set; } = person.BirthDate;
 }
